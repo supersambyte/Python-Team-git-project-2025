4eae0ff8282da333b07e8cdce52243143430c145
supersambyte <omakasammie@gmail.com>
2025-01-12 21:49:42 +0100

    refactored the print line to be one statement

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 def print_hello(name:str) -> str:
     return f"Hello, {name}!"
 
-
-response = print_hello ("Sam")
-
-print(response)
+print(print_hello ("Sam"))
 
 def add(a:int,b:int) -> int:
     return a + b
